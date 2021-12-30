0d059fb265376910fc0e7676201a1619b21d446b
Brice <bricemfrisco@gmail.com>
2021-12-30 17:10:44 -0600

    Added lambda function: Query DynamoDB for specified server between a date range

--- src/functions/pinger.js
+++ src/functions/pinger.js
@@ -1,28 +1,12 @@
 'use strict';
-
-const pinger = require('minecraft-server-ping')
-const dynamo = require('dynamodb')
-const joi = require('joi')
-
-dynamo.AWS.config.update({region: 'us-east-1'})
-
-const TimeSeries = dynamo.define('timeseries', {
-    hashKey: 'n',
-    rangeKey: 't',
-    schema: {
-        n: joi.string().alphanum(),
-        t: joi.number(),
-        v: joi.number()
-    }
-})
-
-TimeSeries.config({tableName: process.env.TIMESERIES_TABLE_NAME})
+const Pinger = require('minecraft-server-ping')
+const TimeSeries = require('../util/dynamo').timeSeries();
 
 module.exports.ping = async (event) => {
     for (const record of event['Records']) {
         try {
             const rec = JSON.parse(record['body'])
-            const data = await pinger.ping(rec.host, rec.port)
+            const data = await Pinger.ping(rec.host, rec.port)
             const timeSeries = new TimeSeries({
                 n: rec.name,
                 t: Date.now(),
