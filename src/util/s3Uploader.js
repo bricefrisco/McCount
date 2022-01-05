9b442db56f9eff033d5175431a123399ebdc44e0
Brice <bricemfrisco@gmail.com>
2022-01-04 19:01:16 -0600

    Remove logging

--- src/util/s3Uploader.js
+++ src/util/s3Uploader.js
@@ -4,7 +4,6 @@ const S3 = new AWS.S3();
 module.exports.uploadFile = async (fileName, data) => {
     return new Promise((res, rej) => {
         const base64Data = data.replace(/^data:image\/\w+;base64,/, '');
-        console.log(base64Data)
         const binaryData = new Buffer(base64Data, 'base64')
 
         S3.putObject({
